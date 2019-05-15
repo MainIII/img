021422b02aab38dbd39c90129dc2ac9582577601
胡杰 <42226864+MainIII@users.noreply.github.com>
2019-05-15 11:19:58 +0800

    CONFIG
    配置文件

---
+++ CONFIG.py
@@ -0,0 +1,18 @@
+# 最后图片的宽
+IMG_WIDTH = 6000
+
+# 每个“块”图片的宽
+SLICE_SIZE = 120
+SLICE_WIDTH = 120
+SLICE_HEIGHT = 120
+
+IMG_DIR = "D:\学习\Python\mPython\huoyin"
+
+# 输出目录
+OUT_DIR = "C:/Users/86139/Desktop/out"
+
+# 差值
+ALLOWED_DIFF = 10000
+
+# 允许一张图片的重复使用次数
+REPEAT = 5
